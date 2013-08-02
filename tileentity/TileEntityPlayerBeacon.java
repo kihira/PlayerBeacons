fb275dacd3141cbf7966f84c96b22efab4b4caf8
Kihira <zoe.kihira.lee@gmail.com>
2013-08-02 15:56:09 +0100

    More work on conductors and level detecting

--- tileentity/TileEntityPlayerBeacon.java
+++ tileentity/TileEntityPlayerBeacon.java
@@ -15,6 +15,8 @@ public class TileEntityPlayerBeacon extends TileEntity {
 	private String owner = " ";
 	private boolean isActive = false;
 	private int badStuff = 0;
+	private int conductors;
+	private int levels;
 
 	@Override
 	public void readFromNBT(NBTTagCompound par1NBTTagCompound) {
@@ -58,34 +60,66 @@ public class TileEntityPlayerBeacon extends TileEntity {
 				//If player head
 				if (skull.getSkullType() == 3) {
 					if (skull.getExtraType().equals(this.owner)) {
-						int beaconBase = 0;
-						//TODO check more the one level
-						for (int i = 0; i < 4; i++) {
-							for (int j = 0; j <4; j++) {
-								if (this.worldObj.getBlockId(this.xCoord-1+i, this.yCoord-1, this.zCoord-1+j) == PlayerBeacons.config.playerBeaconBaseBlockID) beaconBase++;
-							}
-						}
-						if (beaconBase == 9) {
-							EntityPlayer player = this.worldObj.getPlayerEntityByName(skull.getExtraType());
-							if (player != null) {
-								player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 200));
-							}
-						}
-						System.out.println("Detected " + beaconBase + " beacon bases");
-						//Keep this below the beacon base counter so we know what level to look on based on how many levels there are
-						//TODO make this work depending on how many levels there are. Always check the lowest level
-						int conductors = 0;
-						for (int i = 0; i < 4; i++) {
-							for (int j = 0; j < 4; j++) {
-								for (int y = 0; y < 4; y++) {
-									if (this.worldObj.getBlockId(this.xCoord-1+i, this.yCoord+y, this.zCoord-1+j) == PlayerBeacons.config.conductorBlockID) conductors++;
-									else break;
+						//TODO not use an array? Maybe enum?
+						this.levels = 0;
+						for (int i = 1; i <= 4; levels = i++) {
+							int j = this.yCoord - i;
+
+							if (j < 0) break;
+
+							boolean flag = true;
+
+							for (int k = this.xCoord - i; k <= this.xCoord + i && flag; ++k) {
+								for (int l = this.zCoord - i; l <= this.zCoord + i; ++l) {
+									if (!(this.worldObj.getBlockId(k, j, l) == PlayerBeacons.config.playerBeaconBaseBlockID)) {
+										flag = false;
+										break;
+									}
 								}
 							}
+
+							if (!flag) break;
+						}
+						if (levels == 1) {
+							EntityPlayer player = this.worldObj.getPlayerEntityByName(skull.getExtraType());
+							if (player != null) {
+								//Do effects
+							}
+						}
+						System.out.println("Detected " + levels + " beacon levels");
+						//Keep this below the beacon base counter so we know what level to look on based on how many levels there are
+						//TODO implement a better checking system
+						this.conductors = 0;
+						int y = 1;
+						while (this.worldObj.getBlockId(this.xCoord-levels, this.yCoord-levels+y, this.zCoord-levels) == PlayerBeacons.config.conductorBlockID) {
+							conductors++;
+							y++;
+							if (y > 3 + levels) break;
+						}
+						y = 1;
+						while (this.worldObj.getBlockId(this.xCoord+levels, this.yCoord-levels+y, this.zCoord-levels) == PlayerBeacons.config.conductorBlockID) {
+							conductors++;
+							y++;
+							if (y > 3 + levels) break;
+						}
+						y = 1;
+						while (this.worldObj.getBlockId(this.xCoord+levels, this.yCoord-levels+y, this.zCoord+levels) == PlayerBeacons.config.conductorBlockID) {
+							conductors++;
+							y++;
+							if (y > 3 + levels) break;
+						}
+						y = 1;
+						while (this.worldObj.getBlockId(this.xCoord-levels, this.yCoord-levels+y, this.zCoord+levels) == PlayerBeacons.config.conductorBlockID) {
+							conductors++;
+							y++;
+							if (y > 3 + levels) break;
 						}
 						System.out.println("Detected " + conductors + " conductors");
 					}
-					else if (this.worldObj.getTotalWorldTime() %60L == 0) this.worldObj.addWeatherEffect(new EntityLightningBolt(this.worldObj, this.xCoord, this.yCoord, this.zCoord));
+					else if (this.worldObj.getTotalWorldTime() %60L == 0) {
+						this.worldObj.addWeatherEffect(new EntityLightningBolt(this.worldObj, this.xCoord, this.yCoord, this.zCoord));
+						worldObj.destroyBlock(xCoord, yCoord+1, zCoord, false);
+					}
 				}
 			}
 		}
