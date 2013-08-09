3e8d99d184dec44e804f5a3a08e3afee050c27a6
Kihira <zoe.kihira.lee@gmail.com>
2013-08-09 16:16:06 +0100

    Add build file for jenkins

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+MCVERSION="1.6.2"
+MAJOR="1"
+MINOR="0"
+
+if [ -d "builds" ]; then
+  rm -r builds
+fi
+mkdir builds
+cp -ar $JENKINS_HOME/forge $WORKSPACE
+mkdir $WORKSPACE/forge/mcp/src/minecraft/playerbeacons
+cp -ar $WORKSPACE/src/ $WORKSPACE/forge/mcp/src/minecraft/playerbeacons
+cd $WORKSPACE/forge/mcp/
+sh recompile.sh
+sh reobfuscate_srg.sh
+cd $WORKSPACE/forge/mcp/reobf/minecraft
+cp -ar $WORKSPACE/src/assets $WORKSPACE/forge/mcp/src/minecraft/playerbeacons/assets
+zip -r -D -9 $WORKSPACE/builds/PlayerBeacons_$MCVERSION-$MAJOR.$MINOR-build$BUILD_NUMBER.zip *
