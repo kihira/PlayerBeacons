b9b098a8e9e423679ee0416a273da8f4b5d1a9ce
Kihira <zoe.kihira.lee@gmail.com>
2014-02-13 11:12:41 +0000

    Add in Waila support for pylons

--- src/playerbeacons/client/HUDPlayerBeacon.java
+++ src/playerbeacons/client/HUDPlayerBeacon.java
@@ -5,9 +5,13 @@ import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
 import net.minecraft.item.ItemStack;
+import playerbeacons.block.BlockDefiledSoulPylon;
 import playerbeacons.block.BlockPlayerBeacon;
+import playerbeacons.tileentity.TileEntityDefiledSoulPylon;
 import playerbeacons.tileentity.TileEntityPlayerBeacon;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 public class HUDPlayerBeacon implements IWailaDataProvider {
@@ -28,6 +32,16 @@ public class HUDPlayerBeacon implements IWailaDataProvider {
             if (config.getConfig("pb.showowner")) currenttip.add("Owner: " + tileEntityPlayerBeacon.getOwner());
             if (config.getConfig("pb.showcorruption")) currenttip.add("Corruption: " + tileEntityPlayerBeacon.getCorruption());
         }
+        else if (accessor.getTileEntity() instanceof TileEntityDefiledSoulPylon) {
+            TileEntityDefiledSoulPylon tileEntityDefiledSoulPylon = (TileEntityDefiledSoulPylon) accessor.getTileEntity();
+            if (config.getConfig("pb.showcrystaltime") && tileEntityDefiledSoulPylon.getStackInSlot(0) != null) {
+                int timeLeft = (tileEntityDefiledSoulPylon.getStackInSlot(0).getMaxDamage() - tileEntityDefiledSoulPylon.getStackInSlot(0).getItemDamage()) * 2;
+                int hours = ((timeLeft / 60) / 60) % 24;
+                int minutes = (timeLeft / 60) % 60;
+                int seconds = timeLeft % 60;
+                currenttip.add("Time until crystal depletion: " + hours + ":" + minutes + ":" + seconds);
+            }
+        }
         return currenttip;
     }
 
@@ -38,8 +52,10 @@ public class HUDPlayerBeacon implements IWailaDataProvider {
 
     public static void callbackRegister(IWailaRegistrar registrar) {
         registrar.registerBodyProvider(new HUDPlayerBeacon(), BlockPlayerBeacon.class);
+        registrar.registerBodyProvider(new HUDPlayerBeacon(), BlockDefiledSoulPylon.class);
 
         registrar.addConfig("Player Beacons", "pb.showowner", "Show Owner");
         registrar.addConfig("Player Beacons", "pb.showcorruption", "Show Corruption");
+        registrar.addConfig("Player Beacons", "pb.showcrystaltime", "Show time remaining for crystals");
     }
 }
