281e540b8cfd082c2b7d7bee7b65bce41dbdfbab
Kihira <zoe.kihira.lee@gmail.com>
2014-04-02 11:15:32 +0100

    Another fix for the head looking too high

--- src/main/java/kihira/playerbeacons/render/BlockPlayerBeaconRenderer.java
+++ src/main/java/kihira/playerbeacons/render/BlockPlayerBeaconRenderer.java
@@ -85,7 +85,7 @@ public class BlockPlayerBeaconRenderer extends TileEntitySpecialRenderer {
     private void faceEntity(EntityLivingBase par1Entity, double posX, double posY, double posZ) {
         double d0 = par1Entity.posX - posX - par1Entity.width;
         double d2 = par1Entity.posZ - posZ - par1Entity.width;
-        double d1 = par1Entity.posY - (posY) + (double)par1Entity.getEyeHeight() - 1.1F;
+        double d1 = par1Entity.posY - posY - par1Entity.height + (double)par1Entity.getEyeHeight() + 0.1F;
 
         double d3 = (double) MathHelper.sqrt_double(d0 * d0 + d2 * d2);
         float f2 = (float)(Math.atan2(d2, d0) * 180.0D / Math.PI) - 90.0F;
